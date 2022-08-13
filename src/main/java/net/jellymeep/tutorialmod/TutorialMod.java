1daeda15d28c240c20da1f9814714ae8eb5799d9
BuildTools <unconfigured@null.spigotmc.org>
2022-08-12 18:31:57 -1000

    Initial commit

---
+++ src/main/java/net/jellymeep/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.jellymeep.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+	public static final String Mod_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jellymeep/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.jellymeep.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
